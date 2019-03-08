84008797be9f5df2d7a7f0928932fd7e212a548d
Gerson Espinal <gerson.espinal@gmail.com>
2019-03-08 12:12:48 -0500

    Added get route for Console

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/consoles', require('./consoles'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
