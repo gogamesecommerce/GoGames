e4c3664e1c8c3028414d54286027aae90735ef54
Gerson Espinal <gerson.espinal@gmail.com>
2019-03-12 11:15:52 -0400

    created smart allGames Component

---
+++ client/components/allGames.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {getAllGames} from '../store/game'
+import PlayStation from './PlayStation'
+import XboxOne from './XboxOne'
+import Pc from './Pc'
+import NintendoSwitch from './NintendoSwitch'
+
+class Games extends Component {
+  componentDidMount() {
+    this.props.fetchAllGames()
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>All Games</h3>
+        {this.props.games
+          ? (<PlayStation {...this.props} />,
+            <XboxOne {...this.props} />,
+            <Pc {...this.props} />,
+            <NintendoSwitch {...this.props} />)
+          : null}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  games: state.games.games
+})
+
+const mapDispatchToProps = dispatch => ({
+  fetchAllGames: () => {
+    dispatch(getAllGames())
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Games)
