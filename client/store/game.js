9d284f1e77a4f214a07cb3d7bd93162a73d53723
Kevin Cho <kevinhcho27@gmail.com>
2019-03-13 10:30:52 -0400

    Added singleGame to redux store

--- client/store/game.js
+++ client/store/game.js
@@ -1,12 +1,15 @@
 import axios from 'axios'
 
 const GET_GAMES = 'GET_GAMES'
+const GET_SINGLE_GAME = 'GET_SINGLE_GAME'
 
 const initialState = {
-  games: []
+  games: [],
+  singleGame: {}
 }
 
 const getGames = games => ({type: GET_GAMES, games})
+const getSingleGame = game => ({type: GET_SINGLE_GAME, game})
 
 export const getAllGames = () => {
   return async dispatch => {
@@ -21,10 +24,25 @@ export const getAllGames = () => {
   }
 }
 
+export const fetchSingleGame = id => {
+  return async dispatch => {
+    try {
+      const response = await axios.get(`/api/games/${id}`)
+      const message = response.data
+      const action = getSingleGame(message)
+      dispatch(action)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
 export default function(state = initialState, action) {
   switch (action.type) {
     case GET_GAMES:
       return {...state, games: action.games}
+    case GET_SINGLE_GAME:
+      return {...state, singleGame: action.game}
     default:
       return state
   }
